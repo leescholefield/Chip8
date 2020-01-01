94e3856f30dd242c4a0e545f9f9f17aae0fd1928
Lee Scholefield <leescholefield@gmail.com>
2020-01-01 12:50:41 +0000

    Added class fro chip8 and opcodes

---
+++ Chip8/Emulator/Opcodes.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Chip8.Emulator
+{
+    public abstract class Opcodes
+    {
+
+        public static Action<Chip8> ClearScreen
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public static Action<Chip8> ReturnFromSubroutine
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public static Action<Chip8> JumpToAddress
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public static Action<Chip8> CallSubroutine
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public static Action<Chip8> SkipInstructionIfVxEquals
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public static Action<Chip8> SkipInstructionIfVxNotEquals
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public static Action<Chip8> SkipInstructionIfVxEqualsVy
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public static Action<Chip8> SetVxTo
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public static Action<Chip8> AddToVx
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+
+    }
+}
