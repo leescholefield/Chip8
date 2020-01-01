aead3db4c8907d4e6170a68a09429b0546c3d0f9
Lee Scholefield <leescholefield@gmail.com>
2020-01-01 11:55:46 +0000

    Added TODO text file plus Registers class

---
+++ Chip8/Emulator/Registers.cs
@@ -0,0 +1,25 @@
+﻿namespace Chip8.Emulator
+{
+    public class Registers
+    {
+
+        private byte[] V;
+
+        public byte this[int index]
+        {
+            get
+            {
+                return V[index];
+            }
+            set
+            {
+                V[index] = value;
+            }
+        }
+
+        public Registers()
+        {
+            V = new byte[16];
+        }
+    }
+}
