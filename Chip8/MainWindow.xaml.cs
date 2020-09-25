edc98c3b780dbfffdde899d89c5276e261784fd3
Lee Scholefield <leescholefield@gmail.com>
2020-09-25 15:54:10 +0100

    MainWindow : removed keypad and register labels from UI MainWindow.cs : replaced the gamescreen grid with a WriteableBitmap hosted in an image.      : detects keyboard inputs

--- Chip8/MainWindow.xaml.cs
+++ Chip8/MainWindow.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using Chip8.Emulator;
 using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Shapes;
+using System.Windows.Media.Imaging;
 
 namespace Chip8
 {
@@ -15,7 +17,33 @@ namespace Chip8
 
         public Chip8Intepreter Chip8 { get; set; }
 
-        private Grid GameScreen { get; set; }
+        private WriteableBitmap GameScreen { get; set; }
+
+        private static Color PIXEL_ON = Colors.Green;
+        private static Color PIXEL_OFF = Colors.LightGreen;
+
+        /// <summary>
+        /// Maps a Input.Key to a byte value
+        /// </summary>
+        private readonly Dictionary<Key, byte> KeyDictionary = new Dictionary<Key, byte> 
+        {
+                {Key.NumPad0, 0x0 },
+                {Key.NumPad1, 0x1},
+                {Key.NumPad2, 0x2},
+                {Key.NumPad3, 0x3},
+                {Key.NumPad4, 0x4},
+                {Key.NumPad5, 0x5},
+                {Key.NumPad6, 0x6},
+                {Key.NumPad7, 0x7},
+                {Key.NumPad8, 0x8},
+                {Key.NumPad9, 0x9},
+                {Key.C, 0xC},
+                {Key.D, 0xD},
+                {Key.E, 0xE},
+                {Key.F, 0xF},
+                {Key.A, 0xA},
+                {Key.B, 0xB}
+        };
 
         public MainWindow()
         {
@@ -23,32 +51,18 @@ namespace Chip8
             InitializeComponent();
 
             Chip8 = new Chip8Intepreter();
-            Chip8.LoadRom("Roms/Cave.ch8");
+            Chip8.LoadRom("Roms/space_invaders.ch8");
+
             CreateScreen();
-            screenRoot.Children.Add(GameScreen);
-            keypad.Keys = Chip8.KeypadArray;
+            Image i = new Image { Width = 600, Height = 300 };
+            RenderOptions.SetBitmapScalingMode(i, BitmapScalingMode.NearestNeighbor);
+            i.Source = GameScreen;
+            screenRoot.Children.Add(i);
         }
 
         private void CreateScreen()
         {
-            GameScreen = new Grid()
-            {
-                Background = Brushes.White
-            };
-
-            // 64
-            var NUM_COLS = 64;
-            for (int i = 0; i <= NUM_COLS; i++)
-            {
-                GameScreen.ColumnDefinitions.Add(new ColumnDefinition());
-            }
-
-            // 32
-            var NUM_ROWS =  32;
-            for (int i = 0; i <= NUM_ROWS; i++)
-            {
-                GameScreen.RowDefinitions.Add(new RowDefinition());
-            }
+            GameScreen = new WriteableBitmap(64, 34, 50, 50, PixelFormats.Bgr32, null);
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -56,11 +70,27 @@ namespace Chip8
             CompositionTarget.Rendering += CompositionTarget_Rendering;
         }
 
+        private void Key_Down(object sender, KeyEventArgs e)
+        {
+            if (KeyDictionary.TryGetValue(e.Key, out byte pressed))
+            {
+                Chip8.KeypadArray.KeyPressed(pressed);
+            }
+        }
+
+        private void Key_Up(object sender, KeyEventArgs e)
+        {
+            if (KeyDictionary.TryGetValue(e.Key, out byte pressed))
+            {
+                Chip8.KeypadArray.KeyReleased(pressed);
+            }
+        }
+
 
         int frameCounter;
         private void CompositionTarget_Rendering(object sender, EventArgs args)
         {
-            int refreshRate = 60 / 20;
+            int refreshRate = 1; // refreshing every hit
             if (frameCounter % refreshRate == 0)
             {
                 Chip8.Cycle();
@@ -69,55 +99,60 @@ namespace Chip8
                 {
                     DrawScreen();
                     Chip8.Redraw = false;
-                 
                 }
 
+               
                 opcodeLabel.Content = Chip8.Opcode.ToString("X");
                 pcLabel.Content = Chip8.ProgramCounter;
-                indexLabel.Content = Chip8.Index;
-
-                register0.Content = "Register 0: " + Chip8.Registers[0];
-                register1.Content = "Register 1: " + Chip8.Registers[1];
-                register2.Content = "Register 2: " + Chip8.Registers[2];
-                register3.Content = "Register 3: " + Chip8.Registers[3];
-                register4.Content = "Register 4: " + Chip8.Registers[4];
-                register5.Content = "Register 5: " + Chip8.Registers[5];
-                register6.Content = "Register 6: " + Chip8.Registers[6];
-                register7.Content = "Register 7: " + Chip8.Registers[7];
-                register8.Content = "Register 8: " + Chip8.Registers[8];
-                register9.Content = "Register 9: " + Chip8.Registers[9];
-                register10.Content = "Register 10: " + Chip8.Registers[10];
-                register11.Content = "Register 11: " + Chip8.Registers[11];
-                register12.Content = "Register 12: " + Chip8.Registers[12];
-                register13.Content = "Register 13: " + Chip8.Registers[13];
-                register14.Content = "Register 14: " + Chip8.Registers[14];
-                register15.Content = "Register 15: " + Chip8.Registers[15];
             }
             frameCounter++;
         }
 
+        /// <summary>
+        /// Writes the values in <see cref="Chip8Intepreter.GraphicsArray"/> into the <see cref="GameScreen"/>.
+        /// 
+        /// The process for this is somewhat inefficient at the moment, since it will have to loop over the entire GraphicsArray and 
+        /// then set the corresponding pixel color in the GameScreen to <see cref="PIXEL_ON"/> or <see cref="PIXEL_OFF"/>, depending on 
+        /// if the value in GraphicsArray is true or false.
+        /// 
+        /// In the future I would like to have a system where the GraphicsArray keeps track of the pixel positions whose value has changed
+        /// so this function does not have to loop over the entire array.
+        /// </summary>
         private void DrawScreen()
         {
-            for (int i = 0; i < Chip8.GraphicsArray.Length; i++)
+            try
             {
-                if (Chip8.GraphicsArray[i])
-                {
-                    var fill = Chip8.GraphicsArray[i] == true ? Brushes.Black : Brushes.White;
+                // stop back buffer updating
+                GameScreen.Lock();
 
+                for (int i = 0; i < Chip8.GraphicsArray.Length; i++)
+                {
+                    var fill = Chip8.GraphicsArray[i] == true ? PIXEL_ON : PIXEL_OFF;
+
+                    int rgbColor = fill.R << 16;
+                    rgbColor |= fill.G << 8;
+                    rgbColor |= fill.B << 0;
+
+                    // get the column and row in the GameScreen
                     int col = i % 64;
                     int row = i / 64;
 
-                    var rect = new Rectangle
+                    unsafe
                     {
-                        Fill = fill,
-                        Width = 10,
-                        Height = 10
-                    };
+                        IntPtr backBuffer = GameScreen.BackBuffer;
+                        backBuffer += row * GameScreen.BackBufferStride;
+                        backBuffer += col * 4;
 
-                    Grid.SetRow(rect, row);
-                    Grid.SetColumn(rect, col);
-                    GameScreen.Children.Add(rect);
+                        *((int*)backBuffer) = rgbColor;
+                    }
                 }
+                // since we are updating the entire backbuffer we can just set the dirt region to cover the entire screen
+                GameScreen.AddDirtyRect(new Int32Rect(0, 0, GameScreen.PixelWidth, GameScreen.PixelHeight));
+            }
+            finally
+            {
+                // release the back buffer so the screen can be updated
+                GameScreen.Unlock();
             }
         }
     }
